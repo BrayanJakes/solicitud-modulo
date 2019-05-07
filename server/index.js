aec22f14486e31b7b2de040055a87c044d85508e
BrayanJakes <brianjakes22@gmail.com>
2019-05-06 23:36:51 -0400

    prueba2

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 
 const { Mongoose } = require('./database/database');
 
@@ -17,7 +16,12 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
+    next();
+});
 
 //rutas
 
