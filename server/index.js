acf5c4c92dab14e0b9f39d32f381d094b38501ac
BrayanJakes <brianjakes22@gmail.com>
2019-05-07 19:16:55 -0400

    arreglos

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.engine('.hbs', exphbs({
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://solicitu-desingweb.herokuapp.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
      res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
     next();
