67d603abc274a4547942f4f3c03d5c229adb4571
BrayanJakes <brianjakes22@gmail.com>
2019-05-07 22:38:01 -0400

    eee

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.engine('.hbs', exphbs({
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://solicitu-desingweb.herokuapp.com/');
+    res.header('Access-Control-Allow-Origin', 'https://solicitu-desingweb.herokuapp.com');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
